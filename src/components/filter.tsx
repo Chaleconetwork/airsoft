e6814e116c0e975ec0a20981ff337a5ffa33698d
Chaleconetwork <Chaleconetworking@gmail.com>
2024-10-22 10:54:03 -0300

    encontrando el bug

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image"
-import Link from "next/link"
-
 export const Filter = () => {
     return (
         <div className="flex gap-10 p-4 border my-4 bg-white text-sm rounded-lg">
